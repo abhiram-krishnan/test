8ae41ddca25eabc6587ee6cbb3f8a3cd773aaa59
Krishnan Abhiram <kabhira@ffarjo-mobl1.amr.corp.intel.com>
2019-03-21 18:36:59 -0700

    Trying to print git commit ID

--- ci.groovy
+++ ci.groovy
@@ -1,8 +1,10 @@
 node("trigger") {
     echo "This is to test CI"
+    print env
     sh """
 	echo "Printing all environment variables:"
         printenv
+	echo "GIT_COMMIT=" $GIT_COMMIT
     """
 }
 
