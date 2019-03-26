f8f32901abf4185b782a4e2d2fc274964914dec8
Krishnan Abhiram <kabhira@leesung-mobl.amr.corp.intel.com>
2019-03-26 12:01:23 -0700

    Converting to scripted pipeline

--- ci.groovy
+++ ci.groovy
@@ -1,39 +1,32 @@
-pipeline {
-    agent { label 'trigger' }
-    stages {
+node("trigger"){
+        try {
         stage ('Init') {
-            steps {
+            try {
                 echo "Init result: ${currentBuild.result}"
                 echo "Init currentResult: ${currentBuild.currentResult}"
             }
-            post {
-                always {
+            finally {
                     echo "Post-Init result: ${currentBuild.result}"
                     echo "Post-Init currentResult: ${currentBuild.currentResult}"
-                }
             }
         }
         stage ('Build') {
-            steps {
+            try {
                 echo "During Build result: ${currentBuild.result}"
                 echo "During Build currentResult: ${currentBuild.currentResult}"
                 sh 'exit 1'
             }
-            post {
-                always {
+            finally {
                     echo "Post-Build result: ${currentBuild.result}"
                     echo "Post-Build currentResult: ${currentBuild.currentResult}"
-                }
             }
         }
     }
-    post {
-        always {
+    
+    finally {
             echo "Pipeline result: ${currentBuild.result}"
             echo "Pipeline currentResult: ${currentBuild.currentResult}"
-        }
     }
     //commitId = sh(returnStdout: true, script: 'git rev-parse HEAD')
     //echo commitId
 }
-
